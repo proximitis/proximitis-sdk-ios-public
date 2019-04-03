6b1011950a6795020dd6aa5e8c8b10f6a070ad39
Jiří Markalous <jirka@markalous.cz>
2019-04-03 12:58:00 +0200

    Removed pod

--- Example/Pods/Target Support Files/Pods-Proximitis_Tests/Pods-Proximitis_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Proximitis_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Proximitis_Tests
-@end
--- Example/Pods/Target Support Files/Proximitis/Proximitis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Proximitis : NSObject
-@end
-@implementation PodsDummy_Proximitis
-@end
