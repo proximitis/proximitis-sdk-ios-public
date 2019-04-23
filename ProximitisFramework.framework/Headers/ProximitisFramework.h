01e800c841231a4ee9716e6b26805ff5fa7e76a4
Jiří Markalous <jirka@markalous.cz>
2019-04-23 09:33:09 +0200

    Opravena kontrola verze sdk

--- ProximitisFramework.framework/Headers/ProximitisFramework.h
+++ ProximitisFramework.framework/Headers/ProximitisFramework.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double ProximitisFrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char ProximitisFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ProximitisFramework/PublicHeader.h>
-
-
